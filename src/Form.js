56a3290dd0798c43d60bb6c9bb6784ac7fc92643
Akshatpareek07 <pareekakshat07@gmail.com>
2023-09-25 09:52:45 +0530

    Initial setup

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './App.css';
+// import ReactDOM from "react-dom/client"
+
+export default function Form(){
+    return( 
+        <div className="">
+        <button>Add Task</button>
+        <button>Delete Task</button>
+        <button>Update Task</button>
+        </div>
+        
+
+      
+
+    )
+}
